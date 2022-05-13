c7961aa181cece4e8a111ff37d142fb64da24603
Quin Eddy <qkeddy@gmail.com>
2022-05-12 21:36:35 -0400

    Fixed issue with deployment.

--- client/src-sw.js
+++ client/src-sw.js
@@ -2,7 +2,7 @@
 // To actually respond to requests with a cached response, a strategy called StaleWhileRevalidate will be used
 // This strategy will first check the cache for a response, and if it finds one, it will return it.
 
-const { warmStrategyCache } = require("workbox-recipes");
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
 const { CacheFirst } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
@@ -10,11 +10,9 @@ const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 const { StaleWhileRevalidate } = require("workbox-strategies");
 
-
-// The precacheAndRoute() method takes an array of URLs to precache. 
+// The precacheAndRoute() method takes an array of URLs to precache.
 // The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
-
 
 const pageCache = new CacheFirst({
     cacheName: "page-cache",
@@ -35,19 +33,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-
 // Set up asset cache
 registerRoute(
-  // Define the callback function that will filter the requests that need to be cached (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
+    // Define the callback function that will filter the requests that need to be cached (in this case, JS and CSS files)
+    ({ request }) => ["style", "script", "worker"].includes(request.destination),
+    new StaleWhileRevalidate({
+        // Name of the cache storage.
+        cacheName: "asset-cache",
+        plugins: [
+            // This plugin will cache responses with these headers to a maximum-age of 30 days
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+        ],
+    })
 );
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,32 +1,32 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+    openDB("jate", 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains("jate")) {
+                console.log("jate database already exists");
+                return;
+            }
+            db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+            console.log("jate database created");
+        },
+    });
 
 // Export a function we will use to PUT to the database.
 export const putDb = async (content) => {
     console.log("Put to the database");
 
     // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB("jate", 1);
+    const jateDb = await openDB("jate", 1);
 
     // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction("jate", "readwrite");
+    const tx = jateDb.transaction("jate", "readwrite");
 
     // Open up the desired object store.
     const store = tx.objectStore("jate");
 
     // Use the .add() method on the store and pass in the content.
-    const request = store.add({ id:1, jate: content });
+    const request = store.add({ id: 1, jate: content });
 
     // Get confirmation of the request.
     const result = await request;
@@ -35,24 +35,28 @@ export const putDb = async (content) => {
 
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
-  console.log('GET from the database');
+    console.log("GET from the database");
 
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB("jate", 1);
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction("jate", "readonly");
 
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
+    // Open up the desired object store.
+    const store = tx.objectStore("jate");
 
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+    // Use the .getAll() method to get all data in the database.
+    const request = store.getAll();
 
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+    // Get confirmation of the request.
+    const result = await request;
+    if (request) {
+        console.log("result.value", result);
+        return result.value;
+    } else {
+        console.error("DB not available");
+    }
 };
 
 initdb();
